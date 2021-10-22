8815b632bad97d37f3993b51628a7f89f90a41ab
mohamadreza hedayat <mohamadrezahedayat@gmail.com>
2021-10-23 01:57:58 +0330

    update .env config

--- src/shared/apis/server.js
+++ src/shared/apis/server.js
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3300/api/v1/',
+  baseURL: `${process.env.REACT_APP_HOST_ADDRESS}/api/v1/`,
 });
-export const imageAddress = 'http://localhost:3300/img/';
-export const baseURL = 'http://localhost:3300/api/v1';
+export const imageAddress = `${process.env.REACT_APP_HOST_ADDRESS}/img/`;
+export const baseURL = `${process.env.REACT_APP_HOST_ADDRESS}/api/v1`;
 
 export const randomApi = (seed) =>
   `https://avatars.dicebear.com/api/avataaars/${seed}.svg`;
